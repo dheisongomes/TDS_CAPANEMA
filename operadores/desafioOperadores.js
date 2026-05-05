4345321a74fd4bb39141c53ccbc5d5dc5fee7e76
dheisongomes <gomesdheison24@gmail.com>
2026-05-04 21:54:21 -0300

    Operadores em JS

---
+++ operadores/desafioOperadores.js
@@ -0,0 +1,29 @@
+// DESAFIO
+
+/* 
+
+Atalho: SHIFT + ALT + A
+
+01.Utilizando os operadores relacionais, declare 2 variáveis e
+ imprima no output/console 5 valores TRUE e 5 valores FALSE.
+*/
+
+
+console.log(10 > 20); // Maior que
+console.log(10 < 20); // Menor que
+console.log(10 >= 20); // Maior ou igual a
+console.log(10 <= 20); // Menor ou igual a
+console.log(10 != 20); // Diferente
+console.log(10 == 20); // Igual
+
+let Dheison = 21 // Number
+let Gomes = "21" // String
+
+// = atribuição
+// == compara somente o valor
+// === compara valor e tipo
+
+console.log(Dheison != Gomes);
+console.log(Dheison=== Gomes);
+
+
